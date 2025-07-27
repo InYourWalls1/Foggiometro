8073c14b7623d005731f91078f3d866d73cb4d1b
InYourWalls1 <fdr.pautasso@gmail.com>
2025-07-27 02:03:05 +0200

    Add Isiliometro, Kirkometro and Ollollaiometro

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+let destinationLatitude;
+let destinationLongitude;
+
 if (document.title == "Foggiometro"){
-	const destinationLatitude = 41.462299;
-	const destinationLongitude = 15.544744;
+	destinationLatitude = 41.462299;
+	destinationLongitude = 15.544744;
 } else if (document.title == "Bosometro"){
-	const destinationLatitude = 40.2964939254985;
-	const destinationLongitude = 8.499781672343888;
+	destinationLatitude = 40.2964939254985;
+	destinationLongitude = 8.499781672343888;
+} else if (document.title == "Isiliometro"){
+	destinationLatitude = 39.74026952022464;
+	destinationLongitude = 9.10920453422067;
+} else if (document.title == "Kirkometro"){
+	destinationLatitude = 39.2001770149445;
+	destinationLongitude = 9.152413952578316;
+} else if (document.title == "Ollollaiometro"){
+	destinationLatitude = 40.168901524733585;
+	destinationLongitude = 9.179073650351103;
 }
 
 
@@ -23,6 +35,7 @@ function distance(){
 	watchId = navigator.geolocation.watchPosition(
 		function(position) {
 			console.log(position.coords.latitude, position.coords.longitude);
+			console.log(document.title)
 			
 			const lat1 = position.coords.latitude
 			const lon1 = position.coords.longitude
@@ -32,9 +45,6 @@ function distance(){
 			const phi2 = destinationLatitude * Math.PI/180;
 			const delta_phi = (destinationLatitude-lat1) * Math.PI/180;
 			const delta_lambda = (destinationLongitude-lon1) * Math.PI/180;
-			const phi2 =destinationLatitude * Math.PI/180;
-			const delta_phi = (destinationLatitude-lat1) * Math.PI/180;
-			const delta_lambda = (destinationLongitude-lon1) * Math.PI/180;
 
 			const a = Math.sin(delta_phi/2) * Math.sin(delta_phi/2) +
 					  Math.cos(phi1) * Math.cos(phi2) *
@@ -42,7 +52,6 @@ function distance(){
 			const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
 			const d = R * c;
-			
 			
 			const km = (d / 1000).toFixed(2);
 			
